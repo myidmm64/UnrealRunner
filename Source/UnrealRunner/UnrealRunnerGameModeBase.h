0568c9194a363f6bf8ea020abbe80e8abcc45f8c
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-07-05 10:56:30 +0900

    0705_blank 프로젝트 생성

---
+++ Source/UnrealRunner/UnrealRunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALRUNNER_API AUnrealRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
